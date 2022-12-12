7c74c83648e2031c28e13df678d483f1ec9a62a3
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-12 19:03:18 +0530

    Handling Selection

--- src/common/listGroup.jsx
+++ src/common/listGroup.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 const ListGroup = (props) => {
-    const {items, textProperty, valueProperty} = props;
+    const {items, textProperty, valueProperty, onItemSelect, selectedItem} = props;
 
-    return (<ul class="list-group">
-        {items.map(item => <li key={item[valueProperty]} class="list-group-item">{item[textProperty]}</li>)}
+    return (<ul className="list-group">
+        {items.map(item => <li 
+        onClick={()=> onItemSelect(item)} 
+        key={item[valueProperty]} 
+        className={item === selectedItem ? "list-group-item active":"list-group-item"}>{item[textProperty]}</li>)}
     </ul>);
 };
 
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -36,7 +36,7 @@ class Movies extends Component {
     }
 
     handleGenreSelect = genre =>{
-        console.log(genre);
+        this.setState({selectedGenre: genre});
     }
 
     render() { 
@@ -50,6 +50,7 @@ class Movies extends Component {
                 <div className='col-3'>
                     <ListGroup
                      items={this.state.genres} 
+                     selectedItem = {this.state.selectedGenre}
                      onItemSelect={this.handleGenreSelect}/>
                 </div>
                 <div className='col'>
