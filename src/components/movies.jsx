d83dc0ba7d2c77c95eb4c8111c20c2ea908380c3
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-12 22:34:41 +0530

    Adding All Genres

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -15,7 +15,10 @@ class Movies extends Component {
     } ;
 
     componentDidMount(){
-        this.setState({movies: getMovies(), genres: getGenres()});
+
+        const genres = [{name:"All Genres"},...getGenres()]
+
+        this.setState({movies: getMovies(), genres});
     }
 
     handleDelete = (movie) => {
@@ -36,7 +39,7 @@ class Movies extends Component {
     }
 
     handleGenreSelect = genre =>{
-        this.setState({selectedGenre: genre});
+        this.setState({selectedGenre: genre , currentPage:1});
     }
 
     render() { 
@@ -45,7 +48,7 @@ class Movies extends Component {
 
         if (count === 0) return <p>There is no movies in the list</p>;
 
-        const filtered = selectedGenre
+        const filtered = selectedGenre && selectedGenre._id
             ? allMovies.filter(m=> m.genre._id === selectedGenre._id)
             : allMovies;
 
@@ -59,7 +62,7 @@ class Movies extends Component {
                      onItemSelect={this.handleGenreSelect}/>
                 </div>
                 <div className='col'>
-                <p>There are {count} movies in the lsit</p>
+                <p>There are {filtered.length} movies in the lsit</p>
             <table className="table">
                 <thead>
                     <tr>
