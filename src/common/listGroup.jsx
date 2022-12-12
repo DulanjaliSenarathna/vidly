3bdcb199dd08d9c6453627b23ceb62a9ed2c5215
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-12 18:23:48 +0530

    Default Props

--- src/common/listGroup.jsx
+++ src/common/listGroup.jsx
@@ -3,9 +3,14 @@ import React from 'react';
 const ListGroup = (props) => {
     const {items, textProperty, valueProperty} = props;
 
-    return <ul class="list-group">
+    return (<ul class="list-group">
         {items.map(item => <li key={item[valueProperty]} class="list-group-item">{item[textProperty]}</li>)}
-    </ul>
-}
+    </ul>);
+};
+
+ListGroup.defaultProps ={
+    textProperty :"name",
+    valueProperty : "_id"
+};
  
 export default ListGroup;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -50,8 +50,6 @@ class Movies extends Component {
                 <div className='col-3'>
                     <ListGroup
                      items={this.state.genres} 
-                     textProperty = "name"
-                     valueProperty = "_id"
                      onItemSelect={this.handleGenreSelect}/>
                 </div>
                 <div className='col'>
