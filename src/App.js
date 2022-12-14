0f04d03084dd606584614ab752066d96495f6bb9
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-15 01:50:31 +0530

    Adding the NavBar

--- src/App.js
+++ src/App.js
@@ -5,9 +5,14 @@ import {Route, Redirect, Switch} from 'react-router-dom';
 import Customers from './components/customers';
 import Rentals from './components/rentals';
 import NotFound from './components/notFound';
+import NavBar from './components/navBar';
+import React from 'react';
+
 
 function App() {
   return (
+    <React.Fragment>
+      <NavBar/>
     <main className='container'>
       <Switch>
       <Route path="/movies" component={Movies}></Route>
@@ -18,6 +23,7 @@ function App() {
       <Redirect to="not-found"></Redirect>
       </Switch>   
     </main>
+    </React.Fragment>
   );
 }
 
