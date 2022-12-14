207f83e7c0e17bad09368c9fdf9577804865a5de
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-14 15:04:13 +0530

    Extracting TableBody

---
+++ src/common/tableBody.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class TableBody extends Component {
+    state = {  } 
+    render() { 
+
+        const {data, columns} = this.props;
+        return <tbody>
+            {data.map(item =><tr>
+                {columns.map(column => <td></td> )}
+            </tr>)}
+            
+        </tbody>
+    }
+}
+ 
+export default TableBody;
--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,5 +1,6 @@
 import React ,  { Component }from 'react'
 import Like from '../common/like';
+import TableBody from '../common/tableBody';
 import TableHeader from '../common/tableHeader';
 
 class MoviesTable extends Component {
@@ -18,6 +19,7 @@ class MoviesTable extends Component {
 
         return  <table className="table">
         <TableHeader columns={this.columns} sortColumn={sortColumn} onSort={onSort}/>
+        <TableBody data={movies}/>
         <tbody>
             {movies.map(movie =>(
                 <tr key={movie._id}>
