cfdcf72bce2ddaf0d2aa43c9e0a4a60b26bda1f7
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-15 13:41:05 +0530

    Refs

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -2,10 +2,17 @@ import React, { Component } from 'react'
 
 class LoginForm extends Component {
 
+    username = React.createRef();
+
+    componentDidMount(){
+        this.username.current.focus(); //instead of this method, we can use autoFocus in inout field as attribute
+    }
+
     handleSubmit = e => {
         e.preventDefault(); //prevent default full page reloads
 
         //call the server
+        const username = this.username.current.value;
         console.log('submited');
     }
     
@@ -15,7 +22,7 @@ class LoginForm extends Component {
             <form onSubmit={this.handleSubmit}>
                 <div className='form-group'>
                     <label htmlFor='usename'>Username</label>
-                    <input id='usename' type="text" className='form-control' />
+                    <input ref={this.username} id='usename' type="text" className='form-control' />
                 </div>
                 <div className='form-group'>
                     <label htmlFor='password'>Password</label>
