0b3c9c3d81c537fc2219152f27d65c9c077c9b6c
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-16 16:30:52 +0530

    Joi

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Joi from 'joi-browser'
 import Input from './common/input'
 
 class LoginForm extends Component {
@@ -8,7 +9,15 @@ class LoginForm extends Component {
         errors: {}
     };
 
+    schema = {
+        username: Joi.string().required(),
+        password: Joi.string().required()
+    }
+
     validate = () =>{
+
+        const result = Joi.validate(this.state.account,this.schema,{abortEarly: false});
+        console.log(result);
         const errors = {};
 
         const {account} = this.state;
