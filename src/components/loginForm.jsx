219beeb7c5a3e3ecfd2144b800b99e3881e7f6d2
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-20 22:50:09 +0530

    Storing the JWT

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -18,7 +18,9 @@ class LoginForm extends Form {
     doSubmit = async () =>{
         try {
             const {data} = this.state;
-            await login(data.username, data.password);
+           const {data :jwt} = await login(data.username, data.password);
+           localStorage.setItem('token',jwt);//set jwt into browser's local storage
+           this.props.history.push('/');
         } catch (ex) {
             if(ex.response && ex.response.status ===400){
                 const errors = {...this.state.errors};
