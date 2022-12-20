823920c43ea1ab5424f06b5262e59327be69881b
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-20 15:29:05 +0530

    Refactoring

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -3,24 +3,28 @@ import config from '../config.json'
 
 const apiEndpoint = config.apiUrl + '/movies';
 
+function movieUrl(id){
+    return `${apiEndpoint}/${id}`;
+}
+
 export function getMovies(){
     return  http.get(apiEndpoint);
 }
 
 export function getMovie(movieId){
-    return  http.get(apiEndpoint + '/'+ movieId);
+    return  http.get(movieUrl(movieId));
 }
 
 export function saveMovie(movie){
     if(movie._id){
         const body = {...movie};
         delete body._id;
-        return http.put(apiEndpoint + '/' + movie._id, body); //restful api doesn't like body of the request include 'id' property that is also include url too. so we delete id from movie object
+        return http.put(movieUrl(movie._id), body); //restful api doesn't like body of the request include 'id' property that is also include url too. so we delete id from movie object
     }
 
     return http.post(apiEndpoint, movie);
    }
 
 export function deleteMovie(movieId){
-   return http.delete(apiEndpoint + "/" + movieId);
+   return http.delete(movieUrl(movieId));
 }
