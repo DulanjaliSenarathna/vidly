cb3651f168fda5663f3634189f949d241dc406d2
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-20 15:57:31 +0530

    Handling Registration Errors

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -16,7 +16,17 @@ class RegisterForm extends Form {
     };
 
     doSubmit = async () =>{
-      await  userService.register(this.state.data);
+
+        try{
+            await  userService.register(this.state.data);
+        }
+        catch(ex){
+            if(ex.response && ex.response.status ===400){
+                const errors = {...this.state.errors};
+                errors.username = ex.response.data;
+                this.setState({errors});
+            }
+        }
         
     }
     render() { 
