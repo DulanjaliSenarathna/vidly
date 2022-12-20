b94eb1c81096543a5b2976c58dcc8e30210358b1
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-20 14:37:56 +0530

    Saving the Movie

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -12,8 +12,14 @@ export function getMovie(movieId){
 }
 
 export function saveMovie(movie){
-   
-}
+    if(movie._id){
+        const body = {...movie};
+        delete body._id;
+        return http.put(apiEndpoint + '/' + movie._id, body); //restful api doesn't like body of the request include 'id' property that is also include url too. so we delete id from movie object
+    }
+
+    return http.post(apiEndpoint, movie);
+   }
 
 export function deleteMovie(movieId){
    return http.delete(apiEndpoint + "/" + movieId);
