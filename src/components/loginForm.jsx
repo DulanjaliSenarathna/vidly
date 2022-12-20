b5fd27e5b7fe4d361b447f283ed7efa3dc662ca6
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-21 00:21:43 +0530

    Displaying the Current User on NavBar

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -20,7 +20,7 @@ class LoginForm extends Form {
             const {data} = this.state;
            const {data :jwt} = await login(data.username, data.password);
            localStorage.setItem('token',jwt);//set jwt into browser's local storage
-           this.props.history.push('/');
+           window.location = '/';
         } catch (ex) {
             if(ex.response && ex.response.status ===400){
                 const errors = {...this.state.errors};
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link,NavLink} from 'react-router-dom'
 
-const NavBar = () => {
+const NavBar = ({user}) => {
     return ( <nav className="navbar navbar-expand-lg bg-light">
     <div className="container-fluid">
       <Link className="navbar-brand" to="/">Vidly</Link>
@@ -19,12 +19,24 @@ const NavBar = () => {
           <li className="nav-item">
             <NavLink className="nav-link"  to="/rentals">Rentals</NavLink>
           </li>
-          <li className="nav-item">
+         { !user && 
+         <React.Fragment>
+         <li className="nav-item">
             <NavLink className="nav-link"  to="/login">Login</NavLink>
           </li>
           <li className="nav-item">
             <NavLink className="nav-link"  to="/register">Register</NavLink>
           </li>
+          </React.Fragment>}
+          { user && 
+         <React.Fragment>
+         <li className="nav-item">
+            <NavLink className="nav-link"  to="/profile">{user.name}</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link"  to="/logout">Logout</NavLink>
+          </li>
+          </React.Fragment>}
         </ul>
       </div>
     </div>
