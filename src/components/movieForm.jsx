3ea3ec8f045aad17da5adb131f1468fa35458341
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-20 12:11:08 +0530

    Refactoring

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -24,14 +24,16 @@ class MovieForm  extends Form {
         dailyRentalRate: Joi.number().required().min(0).max(10).label('Daily Rental Rate')
      };
 
-     async componentDidMount(){
+     async populateGenres(){
         const {data : genres }  = await getGenres();
         this.setState({genres});
+     }
 
-        const movieId = this.props.match.params.id;
-        if(movieId === 'new') return;
-
+     async populateMovie(){
         try{
+            const movieId = this.props.match.params.id;
+            if(movieId === 'new') return;
+
             const {data:movie} = await getMovie(movieId);
             this.setState({data: this.mapToViewModel(movie)});
         }
@@ -40,9 +42,12 @@ class MovieForm  extends Form {
             if(ex.response && ex.response.status === 404 ) 
               this.props.history.replace('/not-found');
         }
-
-        
      }
+
+     async componentDidMount(){
+        await this.populateGenres();
+        await this.populateMovie(); 
+     };
 
      mapToViewModel(movie){
         return {
