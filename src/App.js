8d8ff800cc75830a34ffe753da05ae84996c4890
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-21 20:33:21 +0530

    Extracting ProtectedRoute

--- src/App.js
+++ src/App.js
@@ -10,9 +10,11 @@ import MovieForm from './components/movieForm';
 import LoginForm from './components/loginForm';
 import RegisterForm from './components/registerForm';
 import Logout from './components/logout';
+import ProtectedRoute from './components/common/protectedRoute';
 import auth from './services/authService';
 import './App.css';
 import 'react-toastify/dist/ReactToastify.css'
+
 
 console.warn = () => {}
 
@@ -38,12 +40,8 @@ class  App extends Component {
       <Route path="/register" component={RegisterForm}></Route>
       <Route path="/login" component={LoginForm}></Route>
       <Route path="/logout" component={Logout}></Route>
-      <Route
-         path="/movies/:id" 
-         render={props => {
-          if (!user) return <Redirect to="/login"/>
-          return <MovieForm {...props}/>
-         }}></Route>
+      <ProtectedRoute
+         path="/movies/:id" component={MovieForm}/>
       <Route 
         path="/movies" 
         render={props => <Movies {...props} user={this.state.user}/>}></Route>
